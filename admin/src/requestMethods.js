c1b5f0f6eec1bb3f11ed02f7b2f89c0dded0f43f
renanconstancio <renanhconstancio@gmail.com>
2022-02-02 14:20:04 -0300

    host base

--- admin/src/requestMethods.js
+++ admin/src/requestMethods.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-// const BASE_URL = "http://localhost:5000/api/";
-const BASE_URL = "http://api.lama.test/api/";
+const BASE_URL = "http://localhost:5000/api/";
+// const BASE_URL = "http://api.lama.test/api/";
 const TOKEN = JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser.accessToken;
 
 export const publicRequest = axios.create({
